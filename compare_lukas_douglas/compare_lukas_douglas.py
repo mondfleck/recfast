afe9aec5690c28751edd68c5225dad76960bf3e0
Peter (Beluga) <88857710+mondfleck@users.noreply.github.com>
2025-05-21 13:28:36 -0400

    TMP chkpt code for comparison

---
+++ compare_lukas_douglas/compare_lukas_douglas.py
@@ -0,0 +1,64 @@
+from pyrecfast import recfast as recfast1pt5
+import numpy as np
+import os
+
+
+def recfast1pt4(Omega_B, Omega_DM, Omega_vac, H_0, T_0, Y_p, He_switch=6):
+    DIR = "./data_recfast_1pt4_comparison"
+    tmpin = os.path.join(DIR, "tmp_recfast.in")
+    tmpout = os.path.join(DIR, "tmp_recfast.out")
+    with open(tmpin, "x") as f:
+        f.write(f"{tmpout}\n")
+        f.write(f"{Omega_B} {Omega_DM} {Omega_vac}\n")
+        f.write(f"{H_0} {T_0} {Y_p}\n")
+        f.write(f"{He_switch}")
+    os.system(f"./recfast_tm < {tmpin}")
+    os.remove(tmpin)
+
+    array = np.genfromtxt(tmpout)
+    z = array[1:,0]
+    x_e = array[1:,1]
+    os.remove(tmpout)
+    return z, x_e
+
+
+Omega_B, Omega_DM, Omega_vac = 0.04, 0.20, 0.76
+H_0, T_0, Y_p = 70, 2.725, 0.25
+
+lukas_H = [0,1]
+lukas_He = range(0,7)
+
+# no H option in 1.4 version recfast (Douglas' version)
+douglas_He = range(0,7)
+
+
+### lukas
+Z = [0]
+desc = 'z'
+X_E = []
+
+for h in lukas_H:
+    for he in lukas_He:
+        Z[0], x_e = recfast1pt5(Omega_B, Omega_DM, Omega_vac, H_0, T_0, Y_p, H_switch=h, He_switch=he)
+        X_E.append(x_e)
+        desc += '    h'+str(h)+'he'+str(he)
+
+A = np.stack(Z + X_E, axis=1)
+print('###################')
+print(A.shape)
+print('###################')
+np.savetxt('recfast1.5_lukas.out', A, header=desc)
+
+
+### douglas
+Z = [0]
+desc = 'z'
+X_E = []
+
+for he in douglas_He:
+    Z[0], x_e = recfast1pt4(Omega_B, Omega_DM, Omega_vac, H_0, T_0, Y_p, He_switch=he)
+    X_E.append(x_e)
+    desc += '    he'+str(he)
+
+A = np.stack(Z + X_E, axis=1)
+np.savetxt('recfast1.4_douglas.out', A, header=desc)
---
+++ compare_lukas_douglas/compare_lukas_douglas.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --account=def-douglas
+#SBATCH --time=00:15:00
+
+# load
+
+module load python/3.10
+source recfast/ENV/bin/activate
+
+#
+
+# run
+echo "Running compare_lukas_douglas.py"
+
+
+srun python compare_lukas_douglas.py
+
+echo "Done compare_lukas_douglas.py"
+
+echo "File for ref:\n\n"
+echo " --------------- \n\n"
+echo "$(<compare_lukas_douglas.py )"
